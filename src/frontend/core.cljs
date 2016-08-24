308f8e0d0be358eac185f3478df2d93b6fb4396f
Brian Hurlow <brian@brianhurlow.com>
2016-08-24 09:18:18 -0400

    s3 rendering

--- src/frontend/core.cljs
+++ src/frontend/core.cljs
@@ -4,16 +4,15 @@
             [om.dom :as dom]))
 
 (enable-console-print!)
-(println "start")
+(println "APP START")
 
-(def app-state (atom {:text "foo"
+(def app-state (atom {:text "loading..."
                       :s3-keys []
                       :selected-image nil}))
 
-(defn cycle-image []
-  (swap! app-state update-in [:selected-image] #(str % "foo")))
 
-(aset js/window "onclick" cycle-image)
+
+;; ===== lib
 
 (defn fetch-images [cb]
   (xhr/send "/imgs"
@@ -23,26 +22,53 @@
             parsed (.parse window.JSON res)]
         (cb parsed)))))
 
-(defn make-s3-url [k])
+(defn make-s3-url [k]
+  ; (println (str "URI" (js/encodeURIComponent k)))
+  (str "http://wordsonwalls.nyc.s3.amazonaws.com/" (js/encodeURIComponent k)))
+
+(defn cycle-image []
+  (swap! app-state assoc :text "loading...")
+  (swap! app-state update-in [:selected-image] #(rand-nth (:s3-keys @app-state))))
+
+;; ===== components
 
 (defn root-component [data owner]
   (reify
     om/IRender
     (render [this]
-      (println data)
-      (if (:selected-image data)
-        (dom/img #js {:src "http://wordsonwalls.nyc.s3.amazonaws.com/AMPLIFY%20LOVE%20DISSIPATE%20HATE.jpg" :width 300 :height 300} nil)
-        (dom/h3 nil "loading..."))
-      (dom/div nil (->> (:s3-keys data) (map (fn [x] (dom/h1 nil x))))))))
+      (dom/div #js {:class "frame"}
+        (when (:text data) (dom/h1 nil (:text data)))
+        (when (:selected-image data)
+          (println "making seslected image url")
+          (println (make-s3-url (:selected-image data)))
+          (dom/div #js {:class "img-box"}
+                   (dom/img #js {:src (make-s3-url (:selected-image data)) 
+                                 :width  500
+                                 :onLoad (swap! app-state assoc :text "nice!")
+                                 :height 500}
+                            nil))) 
+        (dom/footer nil (dom/p nil "APP VERSION 0.1"))))))
+                                 
+
+        ; (dom/div nil (->> (:s3-keys data) (map (fn [x] (dom/h1 nil x)))))))))
 
 (om/root root-component app-state
   {:target (. js/document (getElementById "world"))})
 
-(println "fetching image keys"
-  (fetch-images 
-    (fn [images] 
-      (-> images
-          (js->clj :keywordize-keys true)
-          :Contents
-          (->> (map :Key)
-               (swap! app-state assoc :s3-keys))))))
+;; ===== go!
+
+(println "fetching image keys")
+
+(fetch-images 
+  (fn [images] 
+    (-> images
+        (js->clj :keywordize-keys true)
+        :Contents
+        (->> (map :Key)
+             (swap! app-state assoc :s3-keys)))
+    (swap! app-state assoc :text "click to see an image")
+    (aset js/window "onclick" cycle-image)))
+
+
+
+

50e1c457004376b63165ece3a52d987fc051a0d2
Brian Hurlow <brian@brianhurlow.com>
2016-08-24 10:16:40 -0400

    keys inside

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+```
+./bin/clj scripts/watch.clj
+```
--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var express = require('express')
 var app = express()
 var knox = require('knox')
 
+process.env.AWS_ACCESS_KEY_ID = 'AKIAISOHBJGBRTU3GGXQ'
+process.env.AWS_SECRET_ACCESS_KEY = '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'
+process.env.AWS_REGION = 'us-east-1'
+process.env.AWS_DEFAULT_REGION = 'us-east-1'
+
 var client = knox.createClient({
     key: process.env.AWS_ACCESS_KEY_ID
   , secret: process.env.AWS_SECRET_ACCESS_KEY

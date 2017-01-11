782a62633303ed770841c82223e456d53b155fb6
Brian Hurlow <brian@brianhurlow.com>
2017-01-11 09:56:07 -0500

    clean up

--- pages/index.cljc
+++ pages/index.cljc
@@ -6,6 +6,8 @@
 
 #?(:cljs (enable-console-print!))
 
+;; TODO
+;; mirror should escape this
 (defn initial-state [] 
   #?(:clj (do {:names (read-string 
                         (-> (slurp "pages/names.json")
@@ -14,30 +16,64 @@
                                \? "%3F"})))})))
 
 (def state 
-  (tools/state-atom {}))
+  (tools/state-atom {:visible-img 0
+                     :names []}))
 
 (defn fetch-images [cb]
   #?(:cljs 
      (xhr/send "/imgs"
-      (fn [event]) 
-      (println event)
-      (let [res (-> event .-target .getResponseText)
-            parsed (.parse window.JSON res)]
-        (cb parsed)))))
+      (fn [event] 
+        (println event)
+        (let [res (-> event .-target .getResponseText)
+              parsed (.parse window.JSON res)]
+          (cb parsed))))))
 
 (defn make-url [name]
   (str "http://wordsonwalls.nyc.s3.amazonaws.com/processed/" name))
+
+(defonce timer (atom nil))
+
+(defn inc-visible-img []
+  (swap! state update :visible-img
+         (fn [x] 
+           (println "visible image" x)
+           (mod (inc x) 5))))
+
+(defn trigger-anim []
+  #?(:cljs 
+     (do
+       (if @timer
+         (do (js/clearInterval @timer)
+             (reset! timer nil))
+         (reset! timer
+           (js/setInterval 
+             (fn [] 
+               (println "INCREMENTING VISIBLE")
+               (inc-visible-img))
+             200))))))
+
+
+(defn image-animation []
+  [:div
+   {:on-click trigger-anim}
+   [:h1 "IMAGE ANUM"]
+   (when (:names @state)
+     (doall
+       (for [x (range 10)]
+         [:image {:src (make-url (nth (:names @state) x)) 
+                  :style (when (not= x (:visible-img @state)) {:display "none"})
+                  :key x}])))])
 
 (defn render []
   [:div
     [:div.w6.center.green
       {:on-click #(swap! state conj {:r (rand 100)})}
       [:h1 "WORDS ON WALLS!"]
-      [:div
-        (when (:names @state)
-          (for [x (:names @state)]
-            [:image {:src (make-url x) :key x}]))]]])
+      (image-animation)]])
 
 (tools/inject state #'render)
 
-; #?(:cljs (println "SUP STATE" @state))
+
+
+
+
--- static/pages/index.js
+++ static/pages/index.js
@@ -8,40 +8,63 @@ cljs.core.enable_console_print_BANG_.call(null);
 pages.index.initial_state = (function pages$index$initial_state(){
 return null;
 });
-pages.index.state = mirror.tools.state_atom.call(null,cljs.core.PersistentArrayMap.EMPTY);
+pages.index.state = mirror.tools.state_atom.call(null,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"visible-img","visible-img",697721342),(0),new cljs.core.Keyword(null,"names","names",-1943074658),cljs.core.PersistentVector.EMPTY], null));
 pages.index.fetch_images = (function pages$index$fetch_images(cb){
 return goog.net.XhrIo.send("/imgs",(function (event){
-return null;
-}),cljs.core.println.call(null,pages.index.event),(function (){var res = pages.index.event.target.getResponseText();
+cljs.core.println.call(null,event);
+
+var res = event.target.getResponseText();
 var parsed = window.JSON.parse(res);
 return cb.call(null,parsed);
-})());
+}));
 });
 pages.index.make_url = (function pages$index$make_url(name){
 return [cljs.core.str("http://wordsonwalls.nyc.s3.amazonaws.com/processed/"),cljs.core.str(name)].join('');
 });
-pages.index.render = (function pages$index$render(){
-return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div.w6.center.green","div.w6.center.green",1325377124),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (){
-return cljs.core.swap_BANG_.call(null,pages.index.state,cljs.core.conj,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"r","r",-471384190),cljs.core.rand.call(null,(100))], null));
-})], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",-1896887462),"WORDS ON WALLS!"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),(cljs.core.truth_(new cljs.core.Keyword(null,"names","names",-1943074658).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,pages.index.state)))?(function (){var iter__9040__auto__ = (function pages$index$render_$_iter__870(s__871){
-return (new cljs.core.LazySeq(null,(function (){
-var s__871__$1 = s__871;
-while(true){
-var temp__4657__auto__ = cljs.core.seq.call(null,s__871__$1);
-if(temp__4657__auto__){
-var s__871__$2 = temp__4657__auto__;
-if(cljs.core.chunked_seq_QMARK_.call(null,s__871__$2)){
-var c__9038__auto__ = cljs.core.chunk_first.call(null,s__871__$2);
-var size__9039__auto__ = cljs.core.count.call(null,c__9038__auto__);
-var b__873 = cljs.core.chunk_buffer.call(null,size__9039__auto__);
-if((function (){var i__872 = (0);
-while(true){
-if((i__872 < size__9039__auto__)){
-var x = cljs.core._nth.call(null,c__9038__auto__,i__872);
-cljs.core.chunk_append.call(null,b__873,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"image","image",-58725096),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"src","src",-1651076051),pages.index.make_url.call(null,x),new cljs.core.Keyword(null,"key","key",-1516042587),x], null)], null));
+if(typeof pages.index.timer !== 'undefined'){
+} else {
+pages.index.timer = cljs.core.atom.call(null,null);
+}
+pages.index.inc_visible_img = (function pages$index$inc_visible_img(){
+return cljs.core.swap_BANG_.call(null,pages.index.state,cljs.core.update,new cljs.core.Keyword(null,"visible-img","visible-img",697721342),(function (x){
+cljs.core.println.call(null,"visible image",x);
 
-var G__874 = (i__872 + (1));
-i__872 = G__874;
+return cljs.core.mod.call(null,(x + (1)),(5));
+}));
+});
+pages.index.trigger_anim = (function pages$index$trigger_anim(){
+if(cljs.core.truth_(cljs.core.deref.call(null,pages.index.timer))){
+clearInterval(cljs.core.deref.call(null,pages.index.timer));
+
+return cljs.core.reset_BANG_.call(null,pages.index.timer,null);
+} else {
+return cljs.core.reset_BANG_.call(null,pages.index.timer,setInterval((function (){
+cljs.core.println.call(null,"INCREMENTING VISIBLE");
+
+return pages.index.inc_visible_img.call(null);
+}),(200)));
+}
+});
+pages.index.image_animation = (function pages$index$image_animation(){
+return new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"on-click","on-click",1632826543),pages.index.trigger_anim], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",-1896887462),"IMAGE ANUM"], null),(cljs.core.truth_(new cljs.core.Keyword(null,"names","names",-1943074658).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,pages.index.state)))?cljs.core.doall.call(null,(function (){var iter__9040__auto__ = (function pages$index$image_animation_$_iter__1246(s__1247){
+return (new cljs.core.LazySeq(null,(function (){
+var s__1247__$1 = s__1247;
+while(true){
+var temp__4657__auto__ = cljs.core.seq.call(null,s__1247__$1);
+if(temp__4657__auto__){
+var s__1247__$2 = temp__4657__auto__;
+if(cljs.core.chunked_seq_QMARK_.call(null,s__1247__$2)){
+var c__9038__auto__ = cljs.core.chunk_first.call(null,s__1247__$2);
+var size__9039__auto__ = cljs.core.count.call(null,c__9038__auto__);
+var b__1249 = cljs.core.chunk_buffer.call(null,size__9039__auto__);
+if((function (){var i__1248 = (0);
+while(true){
+if((i__1248 < size__9039__auto__)){
+var x = cljs.core._nth.call(null,c__9038__auto__,i__1248);
+cljs.core.chunk_append.call(null,b__1249,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"image","image",-58725096),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"src","src",-1651076051),pages.index.make_url.call(null,cljs.core.nth.call(null,new cljs.core.Keyword(null,"names","names",-1943074658).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,pages.index.state)),x)),new cljs.core.Keyword(null,"style","style",-496642736),((cljs.core.not_EQ_.call(null,x,new cljs.core.Keyword(null,"visible-img","visible-img",697721342).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,pages.index.state))))?new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"display","display",242065432),"none"], null):null),new cljs.core.Keyword(null,"key","key",-1516042587),x], null)], null));
+
+var G__1250 = (i__1248 + (1));
+i__1248 = G__1250;
 continue;
 } else {
 return true;
@@ -49,13 +72,13 @@ return true;
 break;
 }
 })()){
-return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__873),pages$index$render_$_iter__870.call(null,cljs.core.chunk_rest.call(null,s__871__$2)));
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__1249),pages$index$image_animation_$_iter__1246.call(null,cljs.core.chunk_rest.call(null,s__1247__$2)));
 } else {
-return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__873),null);
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__1249),null);
 }
 } else {
-var x = cljs.core.first.call(null,s__871__$2);
-return cljs.core.cons.call(null,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"image","image",-58725096),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"src","src",-1651076051),pages.index.make_url.call(null,x),new cljs.core.Keyword(null,"key","key",-1516042587),x], null)], null),pages$index$render_$_iter__870.call(null,cljs.core.rest.call(null,s__871__$2)));
+var x = cljs.core.first.call(null,s__1247__$2);
+return cljs.core.cons.call(null,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"image","image",-58725096),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"src","src",-1651076051),pages.index.make_url.call(null,cljs.core.nth.call(null,new cljs.core.Keyword(null,"names","names",-1943074658).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,pages.index.state)),x)),new cljs.core.Keyword(null,"style","style",-496642736),((cljs.core.not_EQ_.call(null,x,new cljs.core.Keyword(null,"visible-img","visible-img",697721342).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,pages.index.state))))?new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"display","display",242065432),"none"], null):null),new cljs.core.Keyword(null,"key","key",-1516042587),x], null)], null),pages$index$image_animation_$_iter__1246.call(null,cljs.core.rest.call(null,s__1247__$2)));
 }
 } else {
 return null;
@@ -64,9 +87,14 @@ break;
 }
 }),null,null));
 });
-return iter__9040__auto__.call(null,new cljs.core.Keyword(null,"names","names",-1943074658).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,pages.index.state)));
-})():null)], null)], null)], null);
+return iter__9040__auto__.call(null,cljs.core.range.call(null,(10)));
+})()):null)], null);
 });
-mirror.tools.inject.call(null,pages.index.state,new cljs.core.Var(function(){return pages.index.render;},new cljs.core.Symbol("pages.index","render","pages.index/render",-1834771087,null),cljs.core.PersistentHashMap.fromArrays([new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Keyword(null,"name","name",1843675177),new cljs.core.Keyword(null,"file","file",-1269645878),new cljs.core.Keyword(null,"end-column","end-column",1425389514),new cljs.core.Keyword(null,"column","column",2078222095),new cljs.core.Keyword(null,"line","line",212345235),new cljs.core.Keyword(null,"end-line","end-line",1837326455),new cljs.core.Keyword(null,"arglists","arglists",1661989754),new cljs.core.Keyword(null,"doc","doc",1913296891),new cljs.core.Keyword(null,"test","test",577538877)],[new cljs.core.Symbol(null,"pages.index","pages.index",-1346419101,null),new cljs.core.Symbol(null,"render","render",232498073,null),"pages/index.cljc",13,1,31,31,cljs.core.list(cljs.core.PersistentVector.EMPTY),null,(cljs.core.truth_(pages.index.render)?pages.index.render.cljs$lang$test:null)])));
+pages.index.render = (function pages$index$render(){
+return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div.w6.center.green","div.w6.center.green",1325377124),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (){
+return cljs.core.swap_BANG_.call(null,pages.index.state,cljs.core.conj,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"r","r",-471384190),cljs.core.rand.call(null,(100))], null));
+})], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",-1896887462),"WORDS ON WALLS!"], null),pages.index.image_animation.call(null)], null)], null);
+});
+mirror.tools.inject.call(null,pages.index.state,new cljs.core.Var(function(){return pages.index.render;},new cljs.core.Symbol("pages.index","render","pages.index/render",-1834771087,null),cljs.core.PersistentHashMap.fromArrays([new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Keyword(null,"name","name",1843675177),new cljs.core.Keyword(null,"file","file",-1269645878),new cljs.core.Keyword(null,"end-column","end-column",1425389514),new cljs.core.Keyword(null,"column","column",2078222095),new cljs.core.Keyword(null,"line","line",212345235),new cljs.core.Keyword(null,"end-line","end-line",1837326455),new cljs.core.Keyword(null,"arglists","arglists",1661989754),new cljs.core.Keyword(null,"doc","doc",1913296891),new cljs.core.Keyword(null,"test","test",577538877)],[new cljs.core.Symbol(null,"pages.index","pages.index",-1346419101,null),new cljs.core.Symbol(null,"render","render",232498073,null),"pages/index.cljc",13,1,67,67,cljs.core.list(cljs.core.PersistentVector.EMPTY),null,(cljs.core.truth_(pages.index.render)?pages.index.render.cljs$lang$test:null)])));
 
 //# sourceMappingURL=index.js.map
